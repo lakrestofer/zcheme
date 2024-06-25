5f39051b116080d7d3af4ffe93ddb3129bfc79fd
lakrestofer <lakrestofer@gmail.com>
2024-06-25 23:15:44 +0200

    add parameterized variant of testing function

--- lexer/src/tests/test.zig
+++ lexer/src/tests/test.zig
@@ -15,6 +15,19 @@ pub fn test_prod(
     try std.testing.expectEqual(expected_end, pos);
 }
 
+// for the productions that require to be parameterized with
+// a comptime_int
+pub fn test_prod_base(
+    prod: *const fn (base: comptime_int, input: []const u8, pos: *usize) bool,
+    base: comptime_int,
+    input: []const u8,
+    expected_end: usize,
+) !void {
+    var pos: usize = 0;
+    try std.testing.expect(prod(base, input, &pos));
+    try std.testing.expectEqual(expected_end, pos);
+}
+
 pub fn test_lexer(input: []const u8, expected: []const Token, options: Lexer.LexerOptions) !void {
     var lexer = Lexer.init(input, options);
     var tokens = std.ArrayList(Token).init(test_allocator);
